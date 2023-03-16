9092b7495890f8466c74cdfe5a0514ebb57068f9
margret739 <margretandalya@gmail.com>
2023-03-16 12:58:36 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
+
 /**
  * main - print "Programming is like building a multilingual puzzle
- * followed by a new line.
+ * followed by a new line."
  * Return: Always 0 (Success)
  */
 
