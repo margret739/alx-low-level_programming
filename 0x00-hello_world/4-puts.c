566b6cfec80c9b9bc56f88e23259b34fd9dec959
margret739 <margretandalya@gmail.com>
2023-03-16 13:31:04 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,15 +3,14 @@
 
 /**
  * main -print "Programming is like building a multilingual puzzle
- * followed by a newline."
- * return 0.
+ * followed by a newline.
+ * return: ALways 0 (success)
  */
 
-int main(void) 
+ int main(void)
 
-{
-
+ {
 	char str[] = "\"Programming is like building a multilingual puzzle";
 	puts(str);
 	return (0);
-}
+ }
