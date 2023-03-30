848855718a24f8512a359bd22a94e975004afeff
margret739 <margretandalya@gmail.com>
2023-03-30 04:35:47 -0700

    string copy 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,9 +12,13 @@
 char *_strncpy(char *dest, char *src, int n)
 
 {
-	int k;
+	int index = 0, src_len = 0;
 
-	for (k = 0; k < n; k++)
-		dest[k] = src[k];
+	while (src[index++])
+		src_len++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
 	return (dest);
 }
