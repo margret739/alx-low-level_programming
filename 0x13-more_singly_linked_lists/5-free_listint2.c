06dc30ce2dc1220c766d9a22916ac5bcfeed258b
margret739 <margretandalya@gmail>
2023-04-30 13:15:18 -0700

    nodeint_end

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * free_listint2 - funtion that frees a list
+ * @head: a pointer to the address
+ * description: sets head to NULL
+ */
+
+void free_listint2(listint_t **head)
+
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+
+	head = NULL;
+}
