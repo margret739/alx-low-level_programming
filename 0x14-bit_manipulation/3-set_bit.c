d683d244262d84356cf613e386459c7394fd273d
margret739 <margretandalya@gmail>
2023-05-23 10:33:36 -0700

    setbit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * set_bit - set a bit
+ * @n: pointer to the number
+ * @index: index of the bit
+ * Return: 1 for success, -1 for failure
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+
+{
+	if (index > 63)
+		return (-1);
+
+	*n = ((1UL << index) | *n);
+	return (1);
+}
